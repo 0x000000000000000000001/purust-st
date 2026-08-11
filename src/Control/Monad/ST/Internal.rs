5577554a8a7d75782069f8cedfbdcf68a9267b7a
0x1 <kevin.francart3@gmail.com>
2026-08-11 12:54:04 +0200

    misc

--- src/Control/Monad/ST/Internal.rs
+++ src/Control/Monad/ST/Internal.rs
@@ -1,7 +1,8 @@
-pub type ST = crate::UnknownType;
-pub fn Control_Monad_ST_Internal_foreach() -> crate::UnknownType { crate::UnknownType::new(0) }
 
 pub fn Control_Monad_ST_Internal_modifyImpl() -> crate::UnknownType { crate::UnknownType::new(0) }
 pub fn Control_Monad_ST_Internal_pure_() -> crate::UnknownType { crate::UnknownType::new(0) }
 pub fn Control_Monad_ST_Internal_run() -> crate::UnknownType { crate::UnknownType::new(0) }
 pub fn Control_Monad_ST_Internal_for() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Control_Monad_ST_Internal_foreach() -> crate::UnknownType { crate::UnknownType::new(0) }
+
+pub fn Control_Monad_ST_Internal_while() -> crate::UnknownType { crate::UnknownType::new(0) }
--- src/Control/Monad/ST/Uncurried.rs
+++ src/Control/Monad/ST/Uncurried.rs
@@ -1,5 +1,4 @@
-
-pub fn Control_Monad_ST_Uncurried_runSTFn3() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Control_Monad_ST_Uncurried_runSTFn2() -> crate::UnknownType { crate::UnknownType::new(0) }
 pub fn Control_Monad_ST_Uncurried_runSTFn1() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Control_Monad_ST_Uncurried_runSTFn2() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Control_Monad_ST_Uncurried_runSTFn3() -> crate::UnknownType { crate::UnknownType::new(0) }
 pub fn Control_Monad_ST_Uncurried_runSTFn4() -> crate::UnknownType { crate::UnknownType::new(0) }
